9a748868390d04dc5b55f19024e3e79722120568
Abhishek <abhisheksinghasr734@gmail.com>
2024-01-03 22:24:27 +0530

    feat:mongoDB atlas

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/db.js";
+
+import "dotenv/config";
+
+connectDB();
+
+/*
+.
+.
+.
+.
+.
+
+import express from "express";
+const app = express();
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("Errr: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+  throw error;
+})();
+*/
